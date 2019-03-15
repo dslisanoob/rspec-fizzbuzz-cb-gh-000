0a9abf4e3417b6de77abd2d858e8933ffc0778b8
Dhruv Gargi <dhruv.gargi@learner.manipal.edu>
2019-03-15 14:51:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,10 @@
 def fizzbuzz(int)
 if int % 3 == 0
   return "Fizz"
-end
 elsif int % 5 == 0
   return "Buzz"
-end
 elsif int % 3 == 0 && int % 5 == 0 
   return "Fizzbuzz"
-end
 else
   return "nil"
-end
 end
